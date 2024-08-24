41d5f12a750acf879656c1e41d6b97b8b84ebf76
Marcel Zapf <zapfmarcel@live.de>
2024-08-24 04:50:59 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Check if SSH host keys are available; if not, generate them
-if [ ! -f /etc/ssh/keys/ssh_host_rsa_key ]; then
-    echo "Generating new SSH host keys."
+# Ensure SSH host keys are available; generate them if they do not exist
+if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+    echo "SSH host keys not found, generating new keys."
     ssh-keygen -A
-    cp /etc/ssh/ssh_host_* /etc/ssh/keys/
-    chown -R root:root /etc/ssh/keys
-    chmod 600 /etc/ssh/keys/*
 fi
 
 # Start SSH service
-/usr/sbin/sshd -D -f /etc/ssh/sshd_config &
+/usr/sbin/sshd -D
 
 # Check if any additional commands were passed and execute them
 if [ "$#" -gt 0 ]; then
